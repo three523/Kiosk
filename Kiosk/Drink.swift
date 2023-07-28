86047f56fcb54ac9c337ffe1257c354d7614eb2d
dohyun <thtree523@gmail.com>
2023-07-28 13:41:37 +0900

    클래스별 파일 분리 및 맥주 장바구니에 추가후 뒤로가기 입력시 잘못입력했다는 버그 수정

---
+++ Kiosk/Drink.swift
@@ -0,0 +1,16 @@
+//
+//  Drink.swift
+//  Kiosk
+//
+//  Created by 김도현 on 2023/07/28.
+//
+
+import Foundation
+
+class Drinks: Food {
+    var takeOut: Bool = true
+    
+    init(name: String, price: Double, description: String, takeOut: Bool) {
+        super.init(name: name, price: price, description: description)
+    }
+}
